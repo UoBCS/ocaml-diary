76ae1ea313b5689f674c5c6ffcae55c57a4a0375
Ossama <ossedb@gmail.com>
2014-10-22 20:59:20 +0200

    Added cumulative_sum, is_upper and is_lower functions

--- basic_exercises.ml
+++ basic_exercises.ml
@@ -1,3 +1,18 @@
+(* Factorial function  *)
 let rec fact n = match n with
   | 0 -> 1
-  | _ -> n * (fact (n - 1));;
+  | _ -> n * (fact (n - 1))
+;;
+
+(* Cumulative sum function *)
+let rec cumulative_sum n = match n with
+  | 1 -> 1
+  | _ -> n + cumulative_sum (n - 1)
+;;
+
+let is_upper c = match c with
+  | 'A'..'Z' -> true
+  | _ -> false
+;;
+
+let is_lower c = not (is_upper c);;
