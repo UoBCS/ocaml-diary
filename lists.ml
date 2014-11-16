a1fa2d1d58f4352969e711b0a07e0dcedf7a51d2
Ossama <ossedb@gmail.com>
2014-11-16 01:32:41 +0000

    Added interleave and every functions

--- lists.ml
+++ lists.ml
@@ -197,23 +197,26 @@ let rec fill x = function
 | n -> let m = fill x (n - 1) in x :: m;;
 
 (* This function takes the first n elements of a list *)
-
 let rec take n xs = match (n, xs) with
   | 0, _       -> []
   | n, x :: xs -> let m = take (n - 1) xs in x :: m;;
 
-take 2 [1;2;3;4;5] = [1; 2];;
-
-
 (* This function removes the first n elements of a list *)
-
-let rec drop n xs = ?
-
-drop 3 [1;2;3;4;5] = [4; 5];;
+let rec drop n xs = match (n, xs) with
+  | 0, _       -> xs
+  | n, []      -> []
+  | n, x :: xs -> 
+     if n <> 0 
+     then drop (n - 1) xs
+     else xs
+;;
 
 (* This function 'rotates' the first n elements from the head to the tail *)
-
-let rec rotate n xs = ?
+let rec rotate n xs = match (n, xs) with
+  | 0, _       -> xs
+  | n, []      -> []
+  | n, x :: xs -> rotate (n - 1) xs @ [x]
+;;
 
 rotate 3 [1;2;3;4;5] = [4; 5; 1; 2; 3];;
 
@@ -231,3 +234,22 @@ take' 3 [1;2;3;4;5] = [3; 4; 5];;
 let rec rotate' n xs = ?
 
 rotate' 2 [1; 2; 3; 4; 5] = [4; 5; 1; 2; 3];;
+
+let every n xs =
+  let rec aux n xs i = match (n, xs) with
+    | 1, _       -> xs
+    | n, []      -> []
+    | n, y :: ys ->
+       if i mod n = 0
+       then y :: (aux n ys (i + 1))
+       else aux n ys (i + 1)
+  in aux n xs 1
+;;
+
+let rec interleave = function
+  | []      -> []
+  | x :: xs ->
+     match x with
+     | []      -> []
+     | y :: ys -> y :: interleave (xs @ [ys])
+;;
